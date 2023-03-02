16ee543aa8521443f5fee8d57ff8d8ba2ee3cabf
RogerAcevedo <Rogeracevedocreate@gmail.com>
2023-03-01 16:15:10 -0800

    color palette change

---
+++ script.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector('.wrapper');
+
+const loginLink = document.querySelector('.login-link');
+
+const registerLink = document.querySelector('.register-link');
+
+const btnPopup = document.querySelector(".btnLogin-popup");
+
+const iconClose = document.querySelector(".icon-close")
+
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+// OPEN LOGIN REG ON LOGIN BUTTON "CLICK"
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+// CLOSE LOGIN REG ON LOGIN BUTTON "CLICK"
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
